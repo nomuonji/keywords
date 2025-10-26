0854738c7a8fede5a47bd89d241f0dd58d6c0205
nomuonji <youphone0613@icloud.com>
2025-10-26 23:37:35 +0900

    fix

--- api/[[...slug]].ts
+++ api/[[...slug]].ts
@@ -1,24 +1,6 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node';
-
-let app: ((req: VercelRequest, res: VercelResponse) => void) | null = null;
-
-function loadApp() {
-  if (!app) {
-    try {
-      // eslint-disable-next-line @typescript-eslint/no-var-requires
-      const mod = require('../dist/server/app');
-      app = mod.default ?? mod;
-      if (typeof app !== 'function') {
-        throw new Error('Express app module did not export a handler function');
-      }
-    } catch (error) {
-      throw new Error('Server bundle not found. Run "npm run build" before deploying.', { cause: error });
-    }
-  }
-  return app;
-}
+import app from '../server/app';
 
 export default function handler(req: VercelRequest, res: VercelResponse): void {
-  const expressApp = loadApp();
-  expressApp(req, res);
+  app(req, res);
 }
