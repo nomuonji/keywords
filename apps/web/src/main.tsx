8cf7d78e820e07a276945e62fa872f97166ff660
nomuonji <63096515+nomuonji@users.noreply.github.com>
2026-09-09 18:53:30 +0900

    refactor: load only core web styles

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -2,6 +2,5 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { App } from './App';
 import './styles.css';
-import './product.css';
-import './acceptance.css';
+import './core-ui.css';
 createRoot(document.getElementById('root')!).render(<React.StrictMode><App/></React.StrictMode>);
