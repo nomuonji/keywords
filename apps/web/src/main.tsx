ff1350a3dd772169eebc5f44e921f1406e07beb2
nomuonji <63096515+nomuonji@users.noreply.github.com>
2026-09-09 18:58:07 +0900

    cleanup: make core ui stylesheet the single web style source

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { App } from './App';
-import './styles.css';
 import './core-ui.css';
 createRoot(document.getElementById('root')!).render(<React.StrictMode><App/></React.StrictMode>);
